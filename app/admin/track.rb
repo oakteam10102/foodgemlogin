2600ecf81d4e0f7b4c5ec7445800ca5be27156de
hirenhsakhiya <hirenh.sakhiya@gmail.com>
2014-11-20 12:42:01 +0530

    description and image addition in track

--- app/admin/track.rb
+++ app/admin/track.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Track do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :name,:description,:image
+  permit_params :name
   #
   # or
   #
