f683d19d4fd10fc6fa8f46f3b88391ba69ee29b8
ved <ved@veds-Mac-Pro-8.local>
2014-11-28 16:43:47 +0530

    added sortable in apreference : Admin

--- app/admin/preference.rb
+++ app/admin/preference.rb
@@ -1,26 +1,14 @@
 ActiveAdmin.register Preference do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
   permit_params :subscription_id, :track_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
 
   index do
     selectable_column
     id_column
-    column "Subscription" do |preference|
+    column "Subscription", :sortable => :subscription_id do |preference|
       preference.subscription.customer
     end
-    column "Track" do |preference|
+    column "Track", :sortable => :track_id  do |preference|
       preference.track.name
     end
 
