325c3cb4f0a16b37452f5f4c86dfc95539825e32
ved <ved@veds-Mac-Pro-8.local>
2014-11-29 11:53:37 +0530

    added validation in preferences :: Admin panel

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,4 +1,18 @@
 class Preference < ActiveRecord::Base
   belongs_to :subscription
   belongs_to :track
+
+  validate :has_at_least_one_subscriber
+  validate :has_at_least_one_track
+
+  private
+
+  def has_at_least_one_subscriber
+    errors[:base] << "Preference should have at least one subscriber" if subscription_id.blank?
+  end
+
+  def has_at_least_one_track
+    errors[:base] << "Preference should have at least one track" if track_id.blank?
+  end
+
 end
