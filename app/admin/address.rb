a0c6f42c0f9bfbbfc3d2a0ee2d59e05589707479
ved <ved@veds-Mac-Pro-8.local>
2014-12-01 16:36:00 +0530

    changes in address.rb

--- app/admin/address.rb
+++ app/admin/address.rb
@@ -1,17 +1,5 @@
 ActiveAdmin.register Address do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
   permit_params :customer_id, :first_name, :last_name, :street_address, :suite, :city, :state, :zip, :phone, :email
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
-  # end
+  
 end
