5b77184d2014d5303d48572bd64e6c253830ada2
Taufiq Muhammadi <waruboy@yahoo.com>
2014-12-07 22:03:47 +0700

    emptying migration

--- db/migrate/20141207120719_change_delivery_date_type_in_delivered_meal.rb
+++ db/migrate/20141207120719_change_delivery_date_type_in_delivered_meal.rb
@@ -1,9 +1,9 @@
 class ChangeDeliveryDateTypeInDeliveredMeal < ActiveRecord::Migration
   def self.up 
-    change_column :delivered_meals, :delivery_date, :datetime 
+    # change_column :delivered_meals, :delivery_date, :datetime 
   end
 
   def self.down
-    change_column :delivered_meals, :delivery_date, :string
+    # change_column :delivered_meals, :delivery_date, :string
   end
 end
